d7281f675543424d76a37f18ea33bf0c2b8b0d0e
youngeunpark <youngeun.park@samsung.com>
2018-06-27 22:36:56 +0900

    [doxygen] update files on kernel_module
    Change-Id: I636c5440baeab4dcedc069d33a84d87eb2133f7b
    Signed-off-by: youngeunpark <youngeun.park@samsung.com>

--- kernel_module/html/search/all_0.js
+++ kernel_module/html/search/all_0.js
@@ -5,5 +5,6 @@ var searchData=
   ['module_5fexit',['module_exit',['../my__mod_8c.html#ae872d5bbea4c8f812e608e8dc7db6074',1,'my_mod.c']]],
   ['module_5finit',['module_init',['../my__mod_8c.html#aaee9b4944367015e0fc61caadc10c0ca',1,'my_mod.c']]],
   ['module_5flicense',['MODULE_LICENSE',['../my__mod_8c.html#ad94b36675e7eb067ea3ce6ff9e244a44',1,'my_mod.c']]],
+  ['module_5fparam',['module_param',['../my__mod_8c.html#aed0ed9139aafb6bc3948e23a4ab3209d',1,'my_mod.c']]],
   ['my_5fmod_2ec',['my_mod.c',['../my__mod_8c.html',1,'']]]
 ];
