4c4eb649cb6fb2c512b8d920db1c96e658830ec1
youngeunpark <youngeun.park@samsung.com>
2018-06-27 22:35:29 +0900

    [kmodule] add module parameters
    Change-Id: Ie3f832f8acf793886cc01ea5d5445489f2793a78
    Signed-off-by: youngeunpark <youngeun.park@samsung.com>

--- kernel_module/my_mod.c
+++ kernel_module/my_mod.c
@@ -12,6 +12,31 @@
 #include <linux/kernel.h>
 
 /**
+ * Static variables
+ */
+static int m_param_int = 1;
+static char m_param_char = 'a';
+static char *m_param_str;
+#define BUF_LEN 100
+static char m_param_strbuf[BUF_LEN];
+
+/**
+ * @return
+ * NONE
+ *
+ * @param
+ * NONE
+ */
+static int my_mod_main(void)
+{
+    printk("m_param_int = %d\n", m_param_int);
+    printk("m_param_char = %c\n", m_param_char);
+    printk("m_param_str = %s\n", m_param_str);
+    printk("m_param_strbuf = %s\n", m_param_strbuf);
+    return 0;
+}
+
+/**
  * @return
  * 0 on success, non-zero on failure
  *
@@ -22,7 +47,8 @@ static int my_mod_init(void)
 {
     printk("my_mod_init is called\n");
 
-    // Return 0 on success
+    my_mod_main();
+
     return 0;
 }
 
@@ -67,4 +93,24 @@ MODULE_AUTHOR("Youngeun Park");
  * Macro for the description of this module
  */
 MODULE_DESCRIPTION("Test module to learn Kernel module API");
+
+/**
+ * @brief
+ * Module parameters definitions
+ *
+ * @Notes
+ * Perms should be 0644.
+ *  Error is caused with 0777, Need to investigate...
+ *
+ * @Usage
+ *  insmod my_mod.ko m_param_int=100 m_param_char=66 m_param_str="panzee" m_param_strbuf="hello"
+ */
+module_param(m_param_int, int, 0644);
+MODULE_PARM_DESC(m_param_int, "Module parameter of int type, m_param_int");
+module_param(m_param_char, byte, 0644);
+MODULE_PARM_DESC(m_param_char, "Module parameter of byte type, m_param_char");
+module_param(m_param_str, charp, 0644);
+MODULE_PARM_DESC(m_param_str, "Module parameter of charp, m_param_str");
+module_param_string(m_param_strbuf, m_param_strbuf, BUF_LEN, 0644);
+MODULE_PARM_DESC(m_param_strbuf, "Module parameter of string, m_param_strbuf");
 
